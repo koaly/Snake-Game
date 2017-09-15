3b40d64259ba8aede927c52c1d2d240e8e09c18e
5910501950 <somapa.sakara@gmail.com>
2017-09-15 10:26:59 +0700

    add keypress and fix out of window

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import arcade.key
+
 DIR_UP = 1
 DIR_RIGHT = 2
 DIR_DOWN = 3
@@ -10,7 +12,7 @@ DIR_OFFSET = { DIR_UP: (0,1),
 
 class Snake:
     BLOCK_SIZE = 16
-    MOVE_WAIT = 0.4
+    MOVE_WAIT = 0.2
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -27,8 +29,12 @@ class Snake:
 
         if self.x > self.world.width:
             self.x = 0
+        elif self.x < 0:
+            self.x = self.world.width
         if self.y > self.world.height:
             self.y = 0
+        elif self.y < 0:
+            self.x = self.world.height
         self.x += self.BLOCK_SIZE * DIR_OFFSET[self.direction][0]
         self.y += self.BLOCK_SIZE * DIR_OFFSET[self.direction][1]
         
@@ -41,7 +47,16 @@ class World:
         self.height = height
  
         self.snake = Snake(self, width // 2, height // 2)
- 
+    
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.RIGHT:
+            self.snake.direction = DIR_RIGHT
+        elif key == arcade.key.UP:
+            self.snake.direction = DIR_UP
+        elif key == arcade.key.DOWN:
+            self.snake.direction = DIR_DOWN
+        elif key == arcade.key.LEFT:
+            self.snake.direction = DIR_LEFT
  
     def update(self, delta):
         self.snake.update(delta)
